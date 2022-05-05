9d0f60e0c6494c5fda3b3525138654213b2991fd
Pedro Barbosa <pedaocds@gmail.com>
2022-05-05 18:48:10 -0300

    Start

---
+++ WebScrepping.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.terabyteshop.com.br/cadeira-gamer'
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+soup.find('div',class_ = 'commerce_columns_item_inner')
+
+
+
+
+
