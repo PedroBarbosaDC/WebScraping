768d27281ab60d5eb2fa11b72d909af4f391f85b
Pedro Barbosa <pedaocds@gmail.com>
2022-05-05 21:07:14 -0300

    Update WebScrepping.py

--- WebScrepping.py
+++ WebScrepping.py
@@ -1,14 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.terabyteshop.com.br/cadeira-gamer'
+url = 'https://www.terabyteshop.com.br'
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
 soup.find('div',class_ = 'commerce_columns_item_inner')
 
-
-
+print(soup)
 
 
