b5b5c5dad97100a2a98cdf7d2b8a51b0fa79e593
Pedro Barbosa <pedaocds@gmail.com>
2022-05-09 19:15:07 -0300

    Update WebScrepping.py

--- WebScrepping.py
+++ WebScrepping.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.terabyteshop.com.br'
+url = 'https://www.python.org/'
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
-soup.find('div',class_ = 'commerce_columns_item_inner')
+soup.find('div',class_ = 'widget-title')
 
+#soup.find_all('div', class_ = 'widget-title').h2.next_sibling
 print(soup)
-
 
